ba1e4c6cd6d983dab96790595678a1d43e740056
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-19 01:53:08 +0600

    load server side data set leftSide componant category get data from express.js server using middleware cors

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]); // kono API data load korer jonno amra state use kori.
+
+    // outside thke data load korer jonno side effct hisebe amra useEffect use kori
+    useEffect(() => {
+        fetch('http://localhost:5000/news-categories')
+            .then(res => res.json())
+            .then(data => setCategories(data));
+    }, [])
+
     return (
         <div>
-            <h2>Left Side Nav</h2>
+            <h2>All Category: {categories.length}</h2>
+            {
+                categories.map(category => <p key={category.id}>
+                    <Link to={`/category/${category.id}`}>{category.name}</Link></p>)
+            }
         </div>
     );
 };
