dbb4e1d2335f1634d27486dc4c7417aa8570963a
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-18 22:23:57 +0600

    create layout, folder structure and basic components

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Copyright by rhcreation@2022</p>
+        </div>
+    );
+};
+
+export default Footer;
